4b7a66e314d444395997690fed78166fe12d83ad
Herminio Cota Perea <herminio.cota@sonorasoft.com>
2018-10-30 17:59:31 -0700

    Add Mkdir class is for mkdir command functionality

---
+++ src/com/rtjvm/scala/opp/commands/Mkdir.scala
@@ -0,0 +1,48 @@
+package com.rtjvm.scala.opp.commands
+
+import com.rtjvm.scala.oop.filesystem.State
+import com.rtjvm.scala.opp.files.Directory
+import com.rtjvm.scala.opp.files.DirEntry
+
+class Mkdir(name: String) extends Command {
+  
+  override def apply(state: State): State = {
+      val wd = state.wd
+      if (wd.hasEntry(name)) {
+        state.setMessage("Entry " + name + " already exists!")
+      } else if (name.contains(Directory.SEPARATOR)) {
+        state.setMessage(name + " must not contain separators!")
+      } else if (checkIllegal(name)) {
+        state.setMessage(name + ": illegal entry name!")
+      } else {
+        doMkdir(state, name)
+      } 
+  }
+  
+  def checkIllegal(name: String): Boolean = {
+    name.contains(".")
+  }
+  
+  def doMkdir(state: State, name: String): State = {
+    def updateStructure(currentDirectory: Directory, path: List[String], newEntry: DirEntry): Directory = {
+      if (path.isEmpty) currentDirectory.addEntry(newEntry)
+      else {
+        val oldEntry = currentDirectory.findEntry(path.head).asDirectory
+        currentDirectory.replaceEntry(oldEntry.name, updateStructure(oldEntry, path.tail, newEntry))
+      }
+    }
+    
+    val wd = state.wd
+    
+    val allDirsInPath = wd.getAllFoldersInPath
+    
+    val newDir = Directory.empty(wd.path, name)
+    
+    val newRoot = updateStructure(state.root, allDirsInPath, newDir)
+    
+    val newWd = newRoot.findDescendant(allDirsInPath)
+    
+    State(newRoot, newWd)
+  }
+  
+}
