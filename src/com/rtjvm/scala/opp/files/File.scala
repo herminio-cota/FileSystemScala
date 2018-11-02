177b98793287718a2324099025b2be13daa63300
Herminio Cota Perea <herminio.cota@sonorasoft.com>
2018-11-02 15:53:18 -0700

    Comlete Echo command

--- src/com/rtjvm/scala/opp/files/File.scala
+++ src/com/rtjvm/scala/opp/files/File.scala
@@ -2,7 +2,7 @@ package com.rtjvm.scala.opp.files
 
 import com.rtjvm.scala.oop.filesystem.FileSystemException
 
-class File(override val parentPath: String, override val name: String) extends DirEntry(parentPath, name){
+class File(override val parentPath: String, override val name: String, contents: String) extends DirEntry(parentPath, name){
   
   def asDirectory: Directory =
     throw new FileSystemException("A file cannot be converter to a directory") 
@@ -15,11 +15,17 @@ class File(override val parentPath: String, override val name: String) extends D
   
   def isFile: Boolean = true
   
+  def appendContents(newContents: String): File =
+    setContents(contents + "\n" + newContents)
+  
+  def setContents(newContents: String) : File = 
+    new File (parentPath, name, newContents)
+  
 }
 
 object File {
   
   def empty(parentPath: String, name: String) =
-    new File(parentPath, name)
+    new File(parentPath, name, "")
   
 }
